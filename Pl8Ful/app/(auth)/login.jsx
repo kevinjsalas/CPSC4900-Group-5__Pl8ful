fd02eddaa04068ef1d6de15db54bd35baef8108b
kevinjsalas <kev4412@gmail.com>
2024-10-08 23:22:55 -0400

    small changes

--- Pl8Ful/app/(auth)/login.jsx
+++ Pl8Ful/app/(auth)/login.jsx
@@ -7,18 +7,6 @@ import { useRouter } from "expo-router";
 import { ImageBackground } from "react-native";
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA7NktjwzA71xt7T4MMY-aE3mOQwvU4d74",
-//     authDomain: "pl8ful-ead31.firebaseapp.com",
-//     projectId: "pl8ful-ead31",
-//     storageBucket: "pl8ful-ead31.appspot.com",
-//     messagingSenderId: "603721789941",
-//     appId: "1:603721789941:web:da50c2f04aef14a25730bc"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-
 export const Register = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
